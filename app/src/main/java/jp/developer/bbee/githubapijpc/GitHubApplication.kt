1a659583fb23cbb104dbb78ef604b6eba346637f
BBee <atsushi.kokubun.47@gmail.com>
2022-09-16 11:27:52 +0900

    Make class extends Application with Hilt annotation

---
+++ app/src/main/java/jp/developer/bbee/githubapijpc/GitHubApplication.kt
@@ -0,0 +1,11 @@
+package jp.developer.bbee.githubapijpc
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Application class with Hilt annotation
+ */
+@HiltAndroidApp
+class GitHubApplication : Application() {
+}
